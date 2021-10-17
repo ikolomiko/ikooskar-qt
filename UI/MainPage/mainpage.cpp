564478eac2a15a198499043298eb49c7d94d7bc4
İlker Avcı <ikolomiko@gmail.com>
2021-10-17 13:26:09 +0300

    Initial efforts on a renewed MainPage

--- UI/MainPage/mainpage.cpp
+++ UI/MainPage/mainpage.cpp
@@ -1,9 +1,6 @@
 #include "mainpage.h"
 #include "ui_mainpage.h"
-#include "DAL/database.h"
-#include <QList>
-#include <QDebug>
-using namespace ikoOSKAR::DAL;
+
 namespace ikoOSKAR {
 namespace UI {
 
@@ -17,66 +14,6 @@ MainPage::MainPage(QWidget *parent) :
 MainPage::~MainPage()
 {
     delete ui;
-}
-
-void MainPage::on_btn_database_clicked()
-{
-    //this->setWindowTitle("Öğrenci işlemleri açılıyor");
-    Database* db = new Database();
-    db->Add(new Student{
-                .number = 123,
-                .firstName = "yeni deneme",
-                .lastName = "yeni soyad",
-                .grade = 11,
-                .section = "a"
-    });
-
-    //db->EndOfTheYear();
-
-
-    /*db->Delete(new Ogrenci{
-                   .Id = 12
-               });*/
-    QList<Student> ogrenciler = *db->GetAllStudents();
-
-    /*
-    Ogrenci *o2 = nullptr;
-    for (int i = 0; i < ogrenciler.length(); i++){
-        if (ogrenciler[i].Ad == "ad1"){
-            o2 = &ogrenciler[i];
-            break;
-        }
-    }
-    o2->Ad = "ad5";
-    db->Update(o2);
-    */
-
-    foreach (Student o, ogrenciler){
-        qDebug() << o.id << o.firstName
-                 << o.grade;
-    }
-
-    //this->setWindowTitle(s);
-    //this->setWindowTitle( db->DatabaseExists() );
-    delete(db);
-}
-
-
-void MainPage::on_btn_newscheme_clicked()
-{
-    setWindowTitle("Yeni sınav düzeni oluşturuluyor...");
-}
-
-
-void MainPage::on_btn_history_clicked()
-{
-    setWindowTitle("Önceki sınav düzenleri görüntüleniyor");
-}
-
-
-void MainPage::on_btn_help_clicked()
-{
-    setWindowTitle("Yardııııımmmmm");
 }
 
 } // namespace UI
--- UI/MainPage/mainpage.h
+++ UI/MainPage/mainpage.h
@@ -17,16 +17,7 @@ class MainPage : public QMainWindow
 public:
     explicit MainPage(QWidget *parent = nullptr);
     ~MainPage();
-    
-private slots:
-    void on_btn_database_clicked();
 
-    void on_btn_newscheme_clicked();
-
-    void on_btn_history_clicked();
-
-    void on_btn_help_clicked();
-    
 private:
     Ui::MainPage *ui;
 };
