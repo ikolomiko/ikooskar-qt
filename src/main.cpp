6c2ee3fd4eb46a85192a78d33dccacec55317817
İlker Avcı <ikolomiko@gmail.com>
2022-06-09 22:36:28 +0300

    Move styles into separate file

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,11 @@
 
 int main(int argc, char* argv[])
 {
+    QFile styles(":/styles.qss");
+    styles.open(QFile::ReadOnly);
+
     QApplication a(argc, argv);
+    a.setStyleSheet(QLatin1String(styles.readAll()));
     ikoOSKAR::UI::MainPage w;
     w.show();
     return a.exec();
