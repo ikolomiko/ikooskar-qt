5e4807153ce6008fa659fa14b7faa17cc798f3ed
İlker Avcı <ikolomiko@gmail.com>
2022-10-31 23:46:39 +0300

    win32: Drop support for 32-bit Win32 target in favor of 64-bit

--- scripts/compile4win32.sh
+++ scripts/compile4win32.sh
@@ -11,10 +11,10 @@ cd temp
 cp -r ../../src .
 cp -r ../../assets .
 cp ../../ikoOSKAR.pro .
-/opt/mxe/usr/bin/i686-w64-mingw32.static-qmake-qt5 && make -j8
+/opt/mxe/usr/bin/x86_64-w64-mingw32.static-qmake-qt5 && make -j8
 mv src/release/ikoOSKAR.exe ../win32/
 cd ..
-cp ./win32/ikoOSKAR.exe $HOME/vm-shared/
+cp ./win32/ikoOSKAR.exe $HOME/vm-shared/ikoOSKAR64.exe
 rm -rf temp
 cd $location
 echo "Took $SECONDS seconds"
