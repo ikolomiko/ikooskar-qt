42d6e8296f2734019328bc26badc97607ee175c4
İlker Avcı <ikolomiko@gmail.com>
2023-07-13 15:23:07 +0300

    multiimportui: implement basics of btnOpenFile

--- src/UI/MultiImportUi/multiimportui.cpp
+++ src/UI/MultiImportUi/multiimportui.cpp
@@ -1,20 +1,75 @@
 #include "multiimportui.h"
 #include "ui_multiimportui.h"
+#include <QDir>
+#include <QFileDialog>
+#include <QStandardPaths>
 
 namespace ikoOSKAR {
 namespace UI {
 
-MultiImportUi::MultiImportUi(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::MultiImportUi)
-{
-    ui->setupUi(this);
-}
+    MultiImportUi::MultiImportUi(QWidget* parent)
+        : QDialog(parent)
+        , ui(new Ui::MultiImportUi)
+    {
+        ui->setupUi(this);
 
-MultiImportUi::~MultiImportUi()
-{
-    delete ui;
-}
+        const QString* errorTitle = new QString("Excel'den Öğrenci Eklerken Hata Oluştu");
+        auto error = new ErrorUi(*errorTitle);
+        bll = new BLL::MultiImportHelper(error);
+    }
+
+    MultiImportUi::~MultiImportUi()
+    {
+        delete this->bll;
+        delete ui;
+    }
+
+    void MultiImportUi::on_btnOpenFile_clicked()
+    {
+        QFileDialog dialog(this);
+        dialog.setFileMode(QFileDialog::ExistingFile);
+        dialog.setNameFilter("E-Okul Excel - Sadece Veri (*.XLS *.xls)");
+        dialog.setViewMode(QFileDialog::Detail);
+        dialog.setDirectory(QStandardPaths::writableLocation(QStandardPaths::DownloadLocation));
+
+        if (dialog.exec() == QDialog::Rejected) {
+            return;
+        }
+
+        QString xlsFilePath = dialog.selectedFiles().at(0);
+
+        // TODO show loading spinner
+
+        auto parsedStudents = bll->parseXls(xlsFilePath);
+
+        // TODO hide loading spinner
+
+        // All necessary error messages have been shown already,
+        // just return without any further action in case of an error
+        if (parsedStudents == nullptr) {
+            return;
+        }
+
+        { // Testing
+            auto s = parsedStudents->at(0);
+            auto inf = s->firstName + "+" + s->lastName + "-" + QString::number(s->id);
+            QMessageBox::information(this, "Success", QString::number(parsedStudents->size()) + " öğrenci bulundu\n" + inf);
+
+            auto s2 = parsedStudents->at(parsedStudents->size() - 1);
+            auto inf2 = s2->firstName + "+" + s2->lastName + "-" + QString::number(s2->id);
+            QMessageBox::information(this, "Success", QString::number(parsedStudents->size()) + " öğrenci bulundu\n" + inf2);
+        }
+
+        // All checks passed. Show another popup window and ask for class/grade.
+        // Or change the root widget instead of a popup window
+        // Show the number of scanned students as well.
+
+        // if dialog.exec == accepted // on_btnsave_click
+        // for each student set classname and section
+        // add them all to the databse
+        // msgbox successfully imported class of 9-a with its 10 students
+        // return qdialog::accepted
+    }
 
 } // namespace UI
 } // namespace ikoOSKAR
--- src/UI/MultiImportUi/multiimportui.h
+++ src/UI/MultiImportUi/multiimportui.h
@@ -1,27 +1,30 @@
 #ifndef IKOOSKAR_UI_MULTIIMPORTUI_H
 #define IKOOSKAR_UI_MULTIIMPORTUI_H
 
+#include "BLL/MultiImportHelper/multiimporthelper.h"
 #include <QDialog>
 
 namespace ikoOSKAR {
 namespace UI {
 
-namespace Ui {
-class MultiImportUi;
-}
+    namespace Ui {
+        class MultiImportUi;
+    }
 
-class MultiImportUi : public QDialog
-{
-    Q_OBJECT
+    class MultiImportUi : public QDialog {
+        Q_OBJECT
 
-public:
-    explicit MultiImportUi(QWidget *parent = nullptr);
-    ~MultiImportUi();
+    public:
+        explicit MultiImportUi(QWidget* parent = nullptr);
+        ~MultiImportUi();
 
-private:
-    Ui::MultiImportUi *ui;
-};
+    private slots:
+        void on_btnOpenFile_clicked();
 
+    private:
+        Ui::MultiImportUi* ui;
+        BLL::MultiImportHelper* bll;
+    };
 
 } // namespace UI
 } // namespace ikoOSKAR
