084548feabd95c95389bfda84de9a1c8f678051c
İlker Avcı <ikolomiko@gmail.com>
2023-07-21 22:43:47 +0300

    miclasspickerui: Add all necessary features
    - Completed the ui for grade and section selection
    - Exposed the previous button for its clicked signal
    - Created a signal for the selected grade and section

--- src/UI/MultiImportUi/miclasspickerui.cpp
+++ src/UI/MultiImportUi/miclasspickerui.cpp
@@ -4,17 +4,27 @@
 namespace ikoOSKAR {
 namespace UI {
 
-MIClassPickerUi::MIClassPickerUi(QWidget *parent) :
-    QWidget(parent),
-    ui(new Ui::MIClassPickerUi)
-{
-    ui->setupUi(this);
-}
+    MIClassPickerUi::MIClassPickerUi(int nParsedStudents, QWidget* parent)
+        : QWidget(parent)
+        , ui(new Ui::MIClassPickerUi)
+    {
+        ui->setupUi(this);
+        QString placeholderTxt = ui->lblDescription->text();
+        placeholderTxt.replace("<n>", QString::number(nParsedStudents));
+        ui->lblDescription->setText(placeholderTxt);
 
-MIClassPickerUi::~MIClassPickerUi()
-{
-    delete ui;
-}
+        btnPrev = ui->btnPrev;
+    }
+
+    MIClassPickerUi::~MIClassPickerUi()
+    {
+        delete ui;
+    }
+
+    void MIClassPickerUi::on_btnNext_clicked()
+    {
+        emit selectedGradeAndSection(ui->spnGrade->value(), ui->cmbSection->currentText());
+    }
 
 } // namespace UI
 } // namespace ikoOSKAR
--- src/UI/MultiImportUi/miclasspickerui.h
+++ src/UI/MultiImportUi/miclasspickerui.h
@@ -1,27 +1,33 @@
 #ifndef IKOOSKAR_UI_MICLASSPICKERUI_H
 #define IKOOSKAR_UI_MICLASSPICKERUI_H
 
+#include <QPushButton>
 #include <QWidget>
 
 namespace ikoOSKAR {
 namespace UI {
+    namespace Ui {
+        class MIClassPickerUi;
+    }
 
-namespace Ui {
-class MIClassPickerUi;
-}
+    class MIClassPickerUi : public QWidget {
+        Q_OBJECT
 
-class MIClassPickerUi : public QWidget
-{
-    Q_OBJECT
+    public:
+        QPushButton* btnPrev;
 
-public:
-    explicit MIClassPickerUi(QWidget *parent = nullptr);
-    ~MIClassPickerUi();
+        explicit MIClassPickerUi(int nParsedStudents, QWidget* parent = nullptr);
+        ~MIClassPickerUi();
 
-private:
-    Ui::MIClassPickerUi *ui;
-};
+    signals:
+        void selectedGradeAndSection(int grade, const QString& section);
 
+    private slots:
+        void on_btnNext_clicked();
+
+    private:
+        Ui::MIClassPickerUi* ui;
+    };
 
 } // namespace UI
 } // namespace ikoOSKAR
