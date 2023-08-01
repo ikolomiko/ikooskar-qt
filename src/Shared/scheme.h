152e1816af23bf3db0580410b4c0a02d765eab60
İlker Avcı <ikolomiko@gmail.com>
2023-08-01 23:38:24 +0300

    shared: create Scheme

---
+++ src/Shared/scheme.h
@@ -0,0 +1,24 @@
+#ifndef IKOOSKAR_SHARED_SCHEME_H
+#define IKOOSKAR_SHARED_SCHEME_H
+
+#include "examstudent.h"
+#include "hall.h"
+#include <QDate>
+#include <QMap>
+#include <QString>
+
+namespace ikoOSKAR {
+namespace Shared {
+
+    class Scheme {
+    public:
+        QString name;
+        QDate date;
+        QMap<QString, QList<ExamStudent>> classLists; // { className: [ExamStudent] }
+        QMap<QString, Hall> hallLayouts; // { hallName: Hall }
+    };
+
+} // namespace Shared
+} // namespace ikoOSKAR
+
+#endif // IKOOSKAR_SHARED_SCHEME_H
