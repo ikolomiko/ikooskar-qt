75e423e9972e02d2962e7a2a52a9c15352939e21
İlker Avcı <ikolomiko@gmail.com>
2023-08-01 23:37:02 +0300

    shared: create Desk

---
+++ src/Shared/desk.h
@@ -0,0 +1,16 @@
+#ifndef IKOOSKAR_SHARED_DESK_H
+#define IKOOSKAR_SHARED_DESK_H
+#include "student.h"
+
+namespace ikoOSKAR {
+namespace Shared {
+
+    class Desk {
+    public:
+        Student* student = nullptr;
+    };
+
+} // namespace Shared
+} // namespace ikoOSKAR
+
+#endif // IKOOSKAR_SHARED_DESK_H
