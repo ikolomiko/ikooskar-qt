2881fa36bf2aa22d95dfec2dac451369e80b051c
İlker Avcı <ikolomiko@gmail.com>
2023-08-13 15:10:08 +0300

    SchemesUi: create XlsxButton widget

---
+++ src/UI/SchemesUi/xlsxbutton.cpp
@@ -0,0 +1,30 @@
+#include "xlsxbutton.h"
+#include "ui_xlsxbutton.h"
+#include <QDesktopServices>
+#include <QUrl>
+
+namespace ikoOSKAR {
+namespace UI {
+
+    XlsxButton::XlsxButton(const QString& name, const QString& filePath, QWidget* parent)
+        : QWidget(parent)
+        , ui(new Ui::XlsxButton)
+        , filePath(filePath)
+    {
+        ui->setupUi(this);
+        ui->btnXlsxFile->setText(name);
+        ui->btnXlsxFile->setIcon(QIcon(":/xlsx.png"));
+    }
+
+    XlsxButton::~XlsxButton()
+    {
+        delete ui;
+    }
+
+    void XlsxButton::on_btnXlsxFile_clicked()
+    {
+        QDesktopServices::openUrl(QUrl::fromLocalFile(filePath));
+    }
+
+} // namespace UI
+} // namespace ikoOSKAR
