bca689827cd9c6ed8d5d958be15c92093dc89cac
İlker Avcı <ikolomiko@gmail.com>
2023-08-19 22:52:56 +0300

    main:Load translator before MainPage() gets called

--- src/main.cpp
+++ src/main.cpp
@@ -10,20 +10,18 @@ int main(int argc, char* argv[])
 {
     QCoreApplication::setOrganizationName("ikoSoft");
     QCoreApplication::setApplicationName("ikoOSKAR");
-
     QSettings().setValue("PATH_DOCS_ROOT", QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "/ikoOSKAR");
 
     QApplication app(argc, argv);
-    ikoOSKAR::UI::MainPage w;
-
     QTranslator baseTranslator;
     if (baseTranslator.load(QLocale::Turkish, QLatin1String("qtbase"), QLatin1String("_"), QLatin1String(":/")))
         QCoreApplication::installTranslator(&baseTranslator);
 
     QFile styles(":/styles.qss");
     styles.open(QFile::ReadOnly);
-
     app.setStyleSheet(QLatin1String(styles.readAll()));
+
+    ikoOSKAR::UI::MainPage w;
     w.show();
     return app.exec();
 }
