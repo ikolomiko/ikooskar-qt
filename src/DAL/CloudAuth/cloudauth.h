998c4a2cf4e1983118b3b4143215d413889733e0
İlker Avcı <ikolomiko@gmail.com>
2023-10-23 16:48:44 +0300

    DAL: create CloudAuth and CloudAuthResponse

---
+++ src/DAL/CloudAuth/cloudauth.h
@@ -0,0 +1,28 @@
+#ifndef IKOOSKAR_DAL_CLOUDAUTH_H
+#define IKOOSKAR_DAL_CLOUDAUTH_H
+
+#include "cloudauthresponse.h"
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QUrl>
+
+namespace ikoOSKAR {
+namespace DAL {
+
+    class CloudAuth {
+    private:
+        const QUrl baseUrl = { "https://api.ikooskar.com.tr" };
+
+        QString getMacAddress();
+        CloudAuthResponse sendRequest(QUrl url);
+
+    public:
+        bool isOnline();
+        CloudAuthResponse setDemoRemainings(int remainings);
+        CloudAuthResponse activateLicense(const QString& license);
+    };
+
+} // namespace DAL
+} // namespace ikoOSKAR
+
+#endif // IKOOSKAR_DAL_CLOUDAUTH_H
