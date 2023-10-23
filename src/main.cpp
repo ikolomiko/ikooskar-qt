fadab629c42765ccc13a8c0de594fb46f75b553e
İlker Avcı <ikolomiko@gmail.com>
2023-10-23 18:09:07 +0300

    app: extract QApp functionality into a new class
    This was required to properly handle BLL::Authenticator signals

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
-#include "UI/MainWindow/mainwindow.h"
+#include "app.h"
 
 #include <QApplication>
-#include <QFile>
 #include <QSettings>
 #include <QStandardPaths>
-#include <QTranslator>
 
 int main(int argc, char* argv[])
 {
@@ -12,16 +10,6 @@ int main(int argc, char* argv[])
     QCoreApplication::setApplicationName("ikoOSKAR");
     QSettings().setValue("PATH_DOCS_ROOT", QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) + "/ikoOSKAR");
 
-    QApplication app(argc, argv);
-    QTranslator baseTranslator;
-    if (baseTranslator.load(QLocale::Turkish, QLatin1String("qtbase"), QLatin1String("_"), QLatin1String(":/")))
-        QCoreApplication::installTranslator(&baseTranslator);
-
-    QFile styles(":/styles.qss");
-    styles.open(QFile::ReadOnly);
-    app.setStyleSheet(QLatin1String(styles.readAll()));
-
-    ikoOSKAR::UI::MainWindow w;
-    w.show();
+    ikoOSKAR::App app(argc, argv);
     return app.exec();
 }
