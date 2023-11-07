cb19215c48e8f1da6cb7e3e9c64463923363e0c1
İlker Avcı <ikolomiko@gmail.com>
2023-11-07 15:24:35 +0300

    c4win32: fix nsis config path

--- scripts/compile4win32.sh
+++ scripts/compile4win32.sh
@@ -21,7 +21,7 @@ fi
 strip -s ikoOSKAR.exe
 
 # Package the binary and create an installer
-x86_64-w64-mingw32.static-makensis windows-installer-config.nsi
+x86_64-w64-mingw32.static-makensis ../scripts/windows-installer-config.nsi
 
 cp ikoOSKAR.exe $HOME/vm-shared/
 cp 'ikoOSKAR v4 Yükleyici.exe' $HOME/vm-shared/
